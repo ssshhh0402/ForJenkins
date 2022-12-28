d2f65fc18c4b95bd3c82acd29b948593aed3123a
ssshhh0402 <ssshhh0402@ex-it.co.kr>
2022-12-28 16:24:54 +0900

    20221228|JenkinsTest6

--- src/main/java/com/example/demo/Ass3Application.java
+++ src/main/java/com/example/demo/Ass3Application.java
@@ -8,6 +8,7 @@ public class Ass3Application {
 	public static void main(String[] args) {
 		System.out.println("Ngrok Testing");
 		System.out.println("Ngrok Testing2");
+		System.out.println("Ngrok Testing3");
 		SpringApplication.run(Ass3Application.class, args);
 	}
 
