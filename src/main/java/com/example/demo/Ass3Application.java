31892e0ad9c3b997a7590f9228030729b0924620
ssshhh0402 <ssshhh0402@ex-it.co.kr>
2022-12-28 11:18:29 +0900

    20221228|JenkinsTesting

--- src/main/java/com/example/demo/Ass3Application.java
+++ src/main/java/com/example/demo/Ass3Application.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Ass3Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Ass3Application.class, args);
 	}
