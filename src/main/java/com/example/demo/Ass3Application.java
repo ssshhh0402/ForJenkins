a5e4fc2ae1b2be66aec77d2adc91afd169860f82
ssshhh0402 <ssshhh0402@ex-it.co.kr>
2022-12-28 16:04:46 +0900

    20221228|JenkinsTest5

--- src/main/java/com/example/demo/Ass3Application.java
+++ src/main/java/com/example/demo/Ass3Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Ass3Application {
 	public static void main(String[] args) {
 		System.out.println("Ngrok Testing");
+		System.out.println("Ngrok Testing2");
 		SpringApplication.run(Ass3Application.class, args);
 	}
 
