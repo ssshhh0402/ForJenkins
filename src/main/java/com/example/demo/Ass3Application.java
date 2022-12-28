b3d232ce0a9cee38bcba21db3d00dc39c55f52de
ssshhh0402 <ssshhh0402@ex-it.co.kr>
2022-12-28 15:49:38 +0900

    20221228|JenkinsTesting

--- src/main/java/com/example/demo/Ass3Application.java
+++ src/main/java/com/example/demo/Ass3Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Ass3Application {
 	public static void main(String[] args) {
+		System.out.println("JenKins Testing");
 		SpringApplication.run(Ass3Application.class, args);
 	}
 
