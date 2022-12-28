43ae311d8d65a961f72925f74aa4c345a6000c33
ssshhh0402 <ssshhh0402@ex-it.co.kr>
2022-12-28 10:36:17 +0900

    Testing

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,5 +2,4 @@ sudo docker ps -a -q --filter "name=192.168.0.0.31:5000/dockertesting3" | grep -
 sudo docker rmi 192.168.0.0.31:5000/dockertesting3
 sudo docker login -u admin -p tnsqja4856 192.168.0.0.31:5000
 sudo docker pull 192.168.0.0.31:5000/dockertesting3
-
 docker run -d -p 8080:8080 --name dockertesting3 192.168.0.0.31:5000/dockertesting3
